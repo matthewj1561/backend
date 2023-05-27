4f260364eb98c55e8e032c813c6d86017e10ac59
Matthew James <matthewj1561@gmail.com>
2023-05-27 11:24:53 -0600

    job posts

---
+++ routes/gigposts.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const gigpostsController = require("../controllers/gigposts");
+
+router.get("/", gigpostsController.getAll);
+router.get("/getlikes", gigpostsController.getLikes);
+router.get("/getcomments", gigpostsController.getComments);
+router.post("/add", gigpostsController.createPost);
+router.put("/addcomment", gigpostsController.addComment);
+router.put("/addlike", gigpostsController.addLike);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-router.use('/user', require('./user'));
-router.use('/posts', require('./posts'))
-router.use('/area', require('./area'))
+router.use("/user", require("./user"));
+router.use("/posts", require("./posts"));
+router.use("/area", require("./area"));
+router.use("/gigposts", require("./gigposts"));
 
 module.exports = router;
